277ed7ff5e2378407b7df2ab0682b9d72fd4e1d7
warriormaster12 <streng.alexander@gmail.com>
2021-01-17 18:13:28 +0200

    fixed some issues with spawning

--- SlimeNation/Ai/Scripts/SpawnPoint.gd
+++ SlimeNation/Ai/Scripts/SpawnPoint.gd
@@ -1,27 +1,29 @@
 extends Position2D
 
 export (PackedScene) var object_to_spawn
+export var random_time_range = {"min": 0.2, "max": 1.0}
 onready var timer = $Timer
-onready var object_instance = object_to_spawn.instance()
+
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	timer.autostart = true
-	timer.wait_time = rand_range(0.2,1.0)
+	timer.wait_time = rand_range(random_time_range.min,random_time_range.max)
 	timer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	print(timer.time_left)
+
 
 
 func _on_Timer_timeout():
-	print("no time left")
 	_spawnCharacter()
+	timer.wait_time = rand_range(random_time_range.min,random_time_range.max)
 	
 	
 func _spawnCharacter():
+	var object_instance = object_to_spawn.instance()
+	object_instance.position = self.position
 	get_parent().add_child(object_instance)
 	
