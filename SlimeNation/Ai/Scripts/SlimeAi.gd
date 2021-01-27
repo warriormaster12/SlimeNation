87921bfc007afc357654ad41a823d1822e57273e
warriormaster12 <streng.alexander@gmail.com>
2021-01-27 19:06:08 +0200

    enemy path follow

--- SlimeNation/Ai/Scripts/SlimeAi.gd
+++ SlimeNation/Ai/Scripts/SlimeAi.gd
@@ -5,6 +5,7 @@ export (int) var speed = 100
 var direction : Vector2
 var velocity : Vector2
 
+onready var path_follow = get_parent()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,11 +18,12 @@ func _ready():
 
 func _physics_process(delta):
 	direction = Vector2()
-	
+	_get_path(delta)
 
-	direction.x += 1 
-	
-	direction = direction.normalized() 
-	velocity = direction * speed * delta
-	
-	move_and_collide(velocity)
+func _get_path(delta):
+	#last position 
+	var prepos = path_follow.get_global_position()
+	path_follow.set_offset(path_follow.get_offset() + speed * delta)
+	#current position 
+	var pos = path_follow.get_global_position()
+	direction = pos
--- SlimeNation/Ai/Scripts/SpawnPoint.gd
+++ SlimeNation/Ai/Scripts/SpawnPoint.gd
@@ -1,16 +1,20 @@
 extends Position2D
 
 export (PackedScene) var object_to_spawn
+export (NodePath) var path
 export var random_time_range = {"min": 0.2, "max": 1.0}
 onready var timer = $Timer
+onready var path_node = get_node(path)
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	timer.autostart = true
-	timer.wait_time = rand_range(random_time_range.min,random_time_range.max)
-	timer.start()
+	_spawn_character()
+	#timer.autostart = true
+	#timer.wait_time = rand_range(random_time_range.min,random_time_range.max)
+	#timer.start()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -25,5 +29,5 @@ func _on_Timer_timeout():
 func _spawn_character():
 	var object_instance = object_to_spawn.instance()
 	object_instance.position = self.position
-	get_parent().add_child(object_instance)
+	path_node.get_child(0).add_child(object_instance)
 	
