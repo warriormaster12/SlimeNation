f6ca7d8d8f2b53f6114b3fc4dc3a6d552052352a
Daniel Riissanen <daniel.riissanen@gmail.com>
2021-01-27 21:16:38 +0200

    feat: Drag camera movement and scrolling

--- SlimeNation/Scripts/Camera.gd
+++ SlimeNation/Scripts/Camera.gd
@@ -1,29 +1,40 @@
 extends Camera2D
 
-const CAMERA_MOVE_THRESHOLD = 0.7;
-const CAMERA_MOVE_SPEED = 200;
 
-# Called when the node enters the scene tree for the first time.
+const ZOOM_STEP = 1.2
+const MAX_ZOOM = pow(ZOOM_STEP, 5)
+const MIN_ZOOM = 1 / pow(ZOOM_STEP, 2)
+
+var _is_dragging: bool = false
+var _drag_start_screen: Vector2 = Vector2()
+var _drag_start_camera: Vector2 = Vector2()
+
+
 func _ready():
 	make_current()
 
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseButton and event.button_index == BUTTON_RIGHT:
+		get_tree().set_input_as_handled()
+		if event.is_pressed():
+			_drag_start_camera = Vector2(position)
+			_drag_start_screen = event.position
+			_is_dragging = true
+		else:
+			_is_dragging = false
+	elif event is InputEventMouseMotion and _is_dragging:
+		get_tree().set_input_as_handled()
+		position = _drag_start_camera - (event.position - _drag_start_screen) * zoom
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
-	var pos = __get_normalized_mouse_pos();
-	if pos.x > CAMERA_MOVE_THRESHOLD:
-		position.x += CAMERA_MOVE_SPEED * delta
-	elif pos.x < -CAMERA_MOVE_THRESHOLD:
-		position.x -= CAMERA_MOVE_SPEED * delta;
+	if event.is_action_pressed("game_camera_zoom_in"):
+		zoom_at_pivot(zoom.x * 1.0 / ZOOM_STEP, get_local_mouse_position())
+	elif event.is_action_pressed("game_camera_zoom_out"):
+		zoom_at_pivot(zoom.x * ZOOM_STEP, get_local_mouse_position())
+	elif event.is_action_pressed("game_camera_zoom_reset"):
+		zoom_at_pivot(1.0, get_local_mouse_position())
 
-	if pos.y > CAMERA_MOVE_THRESHOLD:
-		position.y += CAMERA_MOVE_SPEED * delta
-	elif pos.y < -CAMERA_MOVE_THRESHOLD:
-		position.y -= CAMERA_MOVE_SPEED * delta;
-
-# x: -1 -> 1
-# y: -1 -> 1
-func __get_normalized_mouse_pos() -> Vector2:
-	var pos = get_local_mouse_position()
-	var size = get_viewport_rect().size;
-	return pos / (size / 2.0)
+func zoom_at_pivot(factor: float, pivot: Vector2) -> void:
+	var clampedZ = min(max(factor, MIN_ZOOM), MAX_ZOOM)
+	var new_zoom = Vector2(clampedZ, clampedZ)
+	position = position + pivot * (zoom - new_zoom)
+	zoom = new_zoom
