fdd39edf17cd2478f914c3d45bfb712bde34d845
Daniel Riissanen <daniel.riissanen@gmail.com>
2021-02-24 19:47:39 +0200

    feat: Add time to live timer to Bullet

--- SlimeNation/Entities/Bullet/Bullet.gd
+++ SlimeNation/Entities/Bullet/Bullet.gd
@@ -4,8 +4,15 @@ const SPEED = 500
 const DAMAGE = 7
 var direction = Vector2(0, 0)
 
+# Time to live
+var ttl = 5
+var alive_timer = 0
+
 func _process(delta):
 	position += direction * SPEED * delta
+	alive_timer += delta
+	if alive_timer > ttl:
+		queue_free()
 
 func set_direction(dir: Vector2) -> void:
 	self.direction = dir.normalized()
